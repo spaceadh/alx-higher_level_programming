b6c3e37d54925529809bda35a81f89427038037e
spaceadh <spacesealadhesives13@gmail.com>
2023-09-10 06:08:44 +0300

    Nabado2

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+
 def max_integer(my_list=[]):
-    if my_list == []:
-        return(None)
-    my_list.sort()
-    return(my_list[-1])
+    """
+    Args:
+        my_list: a list
+
+    Returns:
+        the biggest integer in list or none if list is empty
+    """
+
+    if len(my_list) == 0:
+        return None
+    else:
+        biggest_int = my_list[0]
+        for index in range(len(my_list)):
+            if my_list[index] > biggest_int:
+                biggest_int = my_list[index]
+    return biggest_int
