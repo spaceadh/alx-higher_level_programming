7fe20e4f1c342d435d5a5b5a907974d288120905
spaceadh <spacesealadhesives13@gmail.com>
2023-09-10 06:01:28 +0300

    changesHapaNaPale

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,40 +1,41 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
 /**
- * is_palindrome - check if linked list is a palindrome
- * @head: pointer to pointer to head of the list
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * palindrome - check if is palindrome with recursion
+ * @l: l
+ * @r: r
+ *
+ * Return: 1 palindrome, 0 not palindrome
+ */
+int palindrome(listint_t **l, listint_t *r)
+{
+	int response;
+
+	if (r != NULL)
+	{
+		response = palindrome(l, r->next);
+		if (response != 0)
+		{
+			response = (r->n == (*l)->n);
+			*l = (*l)->next;
+			return (response);
+		}
+		return (0);
+
+	}
+	return (1);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: head of linked list
+ *
+ * Return: 1 palindrome, 0 not palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp = *head;
-	int size = sizeof(int);
-	int i = 1;
-	int left = 0;
-	int right = i - 1;
-	int *arr = malloc(100000 * sizeof(int));
-
-	arr[0] = (*head)->n;
-
-	if (*head == NULL)
-		return (1);
-
-	temp = temp->next;
-
-	while (temp != NULL)
+	if (head == NULL)
 	{
-		size += sizeof(int);
-		arr = realloc(arr, size);
-		arr[i] = temp->n;
-		i++;
-		temp = temp->next;
+		return (0);
 	}
-	for (; left < right; left++, right--)
-	{
-		if (arr[left] != arr[right])
-			return (0);
-	}
-	return (1);
+	return (palindrome(head, *head));
 }
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
-        return None
-    max_val = 0
-    for i in my_list:
-        if i > max_val:
-            max_val = i
-    return max_val
+    if my_list == []:
+        return(None)
+    my_list.sort()
+    return(my_list[-1])
