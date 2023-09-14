9d2badb456c4e59243a96ae9156b5482764b7ca9
spaceadh <spacesealadhesives13@gmail.com>
2023-09-14 17:27:59 +0300

    Heh

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -9,5 +9,9 @@
 # -----------------------------------------------------------
 
 
+# def square_matrix_map(matrix=[]):
+#     return list(map(lambda x: list(map(lambda i: i ** 2, x)), matrix))
+
+
 def square_matrix_map(matrix=[]):
-    return list(map(lambda x: list(map(lambda i: i ** 2, x)), matrix))
+    return list(map(lambda x: list(map(lambda y: (y**2), x)), matrix))
