7bfeab5fd5fd049eee2e6a53620d385de19f0589
spaceadh <spacesealadhesives13@gmail.com>
2023-12-12 11:13:20 +0300

    Arrghh

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (let i = 0; i < strings.length; i++) {
-    console.log(strings[i]);
+const arg1 = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < arg1.length; i++) {
+  console.log(arg1[i]);
 }
